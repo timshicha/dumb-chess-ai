14cb78184c5466270baedcfc344e17743a955ef7
dreyso <andreysteb@gmail.com>
2022-06-16 02:13:36 -0700

    Removed this, moved mValue to Piece class and getValue to virtual.

--- src/pieces/pieces.h
+++ src/pieces/pieces.h
@@ -3,55 +3,48 @@
 #include "../chess_pair.h"
 
 #include <memory>
-#include <limits>
+
 
 class Pawn : public Piece
 {
   public:
-    Pawn(Color color, int row, int col, tilesPtr tiles) : Piece{color, row, col, tiles} {}
+    Pawn(Color color, int row, int col, tilesPtr tiles) : Piece{1, color, row, col, tiles} {}
 
     virtual ~Pawn() = default;
 
     bool peekNextPossibleMove() override;
-    int getValue() const;
+    int getValue() const override;
     std::string getName() const override;
-
-  private:
-    int mValue = 1;
 };
 
 
 class Knight : public Piece
 {
   public:
-    Knight(Color color, int row, int col, tilesPtr tiles) : Piece{color, row, col, tiles} {}
+    Knight(Color color, int row, int col, tilesPtr tiles) : Piece{3, color, row, col, tiles} {}
 
     virtual ~Knight() = default;
 
     // Returns a vector of coordinates where the knight can move.
     std::vector<std::vector<int>> getLegalMoves();
     bool peekNextPossibleMove() override;
-    int getValue() const;
+    int getValue() const override;
     std::string getName() const override;
-
-  private:
-    int mValue = 3;
 };
 
 
 class Bishop : public Piece
 {
   public:
-    Bishop(Color color, int row, int col, tilesPtr tiles) : Piece{color, row, col, tiles} {}
+    Bishop(Color color, int row, int col, tilesPtr tiles) : Piece{3, color, row, col, tiles} {}
 
     virtual ~Bishop() = default;
 
     bool peekNextPossibleMove() override;
-    int getValue() const;
+    int getValue() const override;
     std::string getName() const override;
 
   private:
-    int mValue = 3;
     //Movement patterns
     bool mUpLeftSequence = true;
     bool mUpRightSequence = false;
@@ -65,46 +58,40 @@ class Bishop : public Piece
 class Rook : public Piece
 {
   public:
-    Rook(Color color, int row, int col, tilesPtr tiles) : Piece{color, row, col, tiles} {}
+    Rook(Color color, int row, int col, tilesPtr tiles) : Piece{5, color, row, col, tiles} {}
 
     virtual ~Rook() = default;
 
     bool peekNextPossibleMove() override;
-    int getValue() const;
+    int getValue() const override;
     std::string getName() const override;
     // Returns a vector of coordinates where the rook can move.
     std::vector<std::vector<int>> getLegalMoves();
-
-  private:
-    int mValue = 5;
 };
 
 
 class Queen : public Piece
 {
   public:
-    Queen(Color color, int row, int col, tilesPtr tiles) : Piece{color, row, col, tiles} {}
+    Queen(Color color, int row, int col, tilesPtr tiles) : Piece{9, color, row, col, tiles} {}
 
     virtual ~Queen() = default;
 
     bool peekNextPossibleMove() override;
-    int getValue() const;
+    int getValue() const override;
     std::string getName() const override;
-
-  private:
-    int mValue = 9;
 };
 
 
 class King : public Piece
 {
   public:
-    King(Color color, int row, int col, tilesPtr tiles) : Piece{color, row, col, tiles} {}
+    King(Color color, int row, int col, tilesPtr tiles) : Piece{0, color, row, col, tiles} {}
 
     virtual ~King() = default;
 
     bool peekNextPossibleMove() override;
-    int getValue() const;
+    int getValue() const override;
     std::string getName() const override;
 };
 
