deeb6ba84dfb2405b7f8fdce51063b95727b861a
Timofey Shichalin <shichatim@gmail.com>
2022-06-18 00:35:32 -0700

    pawn can now move forward 2

--- src/pieces/pawn.cpp
+++ src/pieces/pawn.cpp
@@ -17,34 +17,50 @@ std::vector<std::pair<int, int>> Pawn::getLegalMoves() const
 
     int newRow, newCol;
     
-    // Can we move forward?
+    // ----- Can we capture to either side? -----
 
-    // If the pawn is white
+    // Increase/decrease row depending on color.
     if(mColor == Color::WHITE)
         newRow = mRow - 1;
-    // If the pawn is black
-    else
+    if(mColor == Color::BLACK)
         newRow = mRow + 1;
-    newCol = mCol;
 
+    newCol = mCol - 1; // Try capturing to the left
+    Tile focusedTile = (*mTiles)[newRow][newCol];
+    // If it's an opponent's piece, we can capture
+    if(focusedTile.getContainedPiece() != nullptr && focusedTile.getContainedPiece()->getColor() != mColor)
+        legalMoves.push_back({newRow, newCol});
+
+    newCol = mCol + 1; // Try capturing to the right
+    focusedTile = (*mTiles)[newRow][newCol];
+    // If it's an opponent's piece, we can capture
+    if(focusedTile.getContainedPiece() != nullptr && focusedTile.getContainedPiece()->getColor() != mColor)
+        legalMoves.push_back({newRow, newCol});
+
+    // ----- Can we move forward? -----
+
+    newCol = mCol;
     // If the new square is empty, it's a legal move
     if((*mTiles)[newRow][newCol].getContainedPiece() == nullptr)
+    {
         legalMoves.push_back({newRow, newCol});
 
-    // Can we capture (forward and to the decreasing columns)?
-    // There must be a piece of the opposite color in that square.
-    newCol = mCol - 1;
-    if((*mTiles)[newRow][newCol].getContainedPiece() != nullptr &&
-        (*mTiles)[newRow][newCol].getContainedPiece()->getColor() != mColor)
-        legalMoves.push_back({newRow, newCol});
-    
-    // Can we capture (forward and to the increasing columns)?
-    // There must be a piece of the opposite color in that square.
-    newCol = mCol + 1;
-    if((*mTiles)[newRow][newCol].getContainedPiece() != nullptr &&
-        (*mTiles)[newRow][newCol].getContainedPiece()->getColor() != mColor)
-        legalMoves.push_back({newRow, newCol});
-
+        // If that was a legal move, see if we can move twice.
+        // We can move twice if the pawn is at its starting position
+        // and both tiles in front of it are free.
+        if(mColor == Color::WHITE && mRow == 6) // If white and in original row
+        {
+            newRow = mRow - 2; //Move twice
+            if((*mTiles)[newRow][newCol].getContainedPiece() == nullptr) // If still empty, we can move
+                legalMoves.push_back({newRow, newCol});
+        }
+        else if(mColor == Color::BLACK && mCol == 1) // If black and in original row
+        {
+            newRow = mRow + 2;
+            if((*mTiles)[newRow][newCol].getContainedPiece() == nullptr) // If still empty, we can move
+                legalMoves.push_back({newRow, newCol});
+        }
+    }
     return legalMoves;
 }
 
