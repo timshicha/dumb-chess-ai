c4eba2db8dec0ebc3f5bb175a28677ebbae723f6
dreyso <andreysteb@gmail.com>
2022-07-08 21:18:27 -0700

    new chess_board plan

--- src/chess_board.h
+++ src/chess_board.h
@@ -7,9 +7,7 @@
 #include <unordered_map>
 #include <stack>
 #include <vector>
-#include <tuple>
-
-
+#include <array>
 
 
 class ChessBoard
@@ -18,9 +16,11 @@ class ChessBoard
     // Create starting chess board
     ChessBoard();
     // Create chessboard as specified by textfile (for testing).
-    // White is to start by default.
-    ChessBoard(const char * filename, Color turnColor = Color::WHITE);
+    ChessBoard(const char * filename);
     ~ChessBoard() = default;
+
+    //Returns an array of pieces of the specified color
+     std::array<Piece, 16>& operator[](Color);
 
     // Nice visualization tool for debugging
     std::string get_board_as_string();
@@ -28,39 +28,35 @@ class ChessBoard
     void test();
     bool isInCheck(Color kingColor) const;
 
-    // Called by user
+    //Can only be used when the states stack is empty
     bool finalMove(int oldRow, int oldCol, int newRow, int newCol);
-    //Called by AI
-    void finalMove(std::unordered_map<Piece*, Tile*> boardSnapshot);
+
+    //Pushes on the stack
+    bool tempMove(int oldRow, int oldCol, int newRow, int newCol);
+
+    //Pops off the stack
+    bool undoTempMove();
+
+    //Returns how many states are on the stack
+    int countTempMoves();
+
+    //Throws error if the wrong color is passed in
+    std::vector<std::pair<int,int>> getLegalMoves(Color pieceColor, int pieceIndex);
 
   private:
     //64 total tiles on the board
     Tile mTiles[8][8];
 
     //16 pieces of each color
-    std::unique_ptr<Piece> mPieces[2][16]; // Access example: mPieces[int(Color::WHITE)][3]
+    std::array<std::array<std::unique_ptr<Piece>, 16>,2> mPieces; // Access example: mPieces[int(Color::WHITE)][3]
+
     //Extra non-owning reference to the kings
     Piece* mKings[2]; // Access example: mKings[int(Color::WHITE)]
-
-    //Cycles through moves within the current board state
-    //Cannot be called successively in a single state, move must be undone before next move is done
-    //Also, cannot be called if the states stack is empty
-    bool tempMove();
-    bool undoTempMove();
-
-    //Pushes a state on the stack
-    bool rememberState();
-
-    //Pops off a state, doesn't care if there is an ongoing move in the current state
-    bool revertState();
-
-    //Pops off all states
-    void revertToOriginalState();
-
+    
+    //Keeps track of the color, affected by temp and final moves
+    Color mTurnColor = Color::WHITE;
+    
     //Remember board states
     //First state is always the actual state of the board
-    std::stack<std::unordered_map<Piece*, Tile*>> states;
-    //Remember where the last tempMove left off for each state
-    //Store the color of the temporarily moved piece, its index, its possible moves and which move is next
-    std::stack<std::tuple<Color, int, std::vector<std::pair<int,int>>, int>> stateTempMovesInfo;
+    std::stack<std::unordered_map<Piece*, Tile*>> mStates;
 };
