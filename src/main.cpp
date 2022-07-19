ed31cdca565ddcda9f6a60c43acd5a245b2183ab
dreyso <andreysteb@gmail.com>
2022-07-18 21:48:05 -0700

    Removed testing stuff

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,10 @@
 
 using namespace std;
 
-// temp function
-bool askMove(ChessBoard &board)
-{
-    int oldRow, oldCol, newRow, newCol;
-    cout << "Old row: ";
-    cin >> oldRow; cin.ignore(100, '\n');
-    cout << "Old column: ";
-    cin >> oldCol; cin.ignore(100, '\n');
-    cout << "New row: ";
-    cin >> newRow; cin.ignore(100, '\n');
-    cout << "New column: ";
-    cin >> newCol; cin.ignore(100, '\n');
-
-    // Try moving
-    return board.safeMove(oldRow, oldCol, newRow, newCol);
-}
-
 int main(int argc, char* argv[])
 {
-    ChessBoard test ("default.txt");
+    ChessBoard test;
     cout << test.get_board_as_string();
-
-    //test.test();
-
-    int turn = 0;
-
-    while(true)
-    {
-        cout << "It's " << turn << "'s turn:\n";
-        while(askMove(test) == false)
-        {
-            cout << "Illegal move. Try again:\n";
-        }
-        cout << test.get_board_as_string();
-
-        turn = !turn;
-    }
     
     return 0;
 }
